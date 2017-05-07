47c600aa8515ed22e25ec07ee0020d82f47d4798
Charlie Steele <steelech@umich.edu>
2017-05-06 23:55:55 -0400

    enable cors

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var app = express();
 app.use(bodyParser.json());
 var port = process.argv[2] || 8888;
 var routes = require('./routes');
+var cors = require('cors');
 
 app.use("/", routes);
+app.use(cors());
 
 console.log("backend server listening at port ", port);
 app.listen(port);
