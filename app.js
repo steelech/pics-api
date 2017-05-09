0c1cbf0ddecabf60bf9f9cd6f89a47f979c0c5b6
Charlie Steele <steelech@umich.edu>
2017-05-09 02:48:10 -0400

    only set necessary headers for cors

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var app = express();
 app.use(bodyParser.json());
 var port = process.argv[2] || 8888;
 var routes = require('./routes');
-var cors = require('cors');
-
 var logger = function(req, res, next) {
 	console.log("REQUEST: ", req.method + " " + req.url + " " + JSON.stringify(req.headers));
 	next();
 }
 
+// enable cors for requests from frontend
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+})
 app.use(logger);
 app.use("/", routes);
-app.use(cors());
 
 console.log("backend server listening at port ", port);
 app.listen(port);
