bf8c648cf1a56467b032d5953092879f197cf92e
Charlie Steele <steelech@umich.edu>
2017-06-18 14:35:03 -0400

    refactoring/cleaning up

--- routes/pics/index.js
+++ routes/pics/index.js
@@ -4,33 +4,43 @@ var AWS = require('aws-sdk');
 module.exports = pics;
 
 pics.post("/", (req, res) => {
-	res.status(202).json({ message: "wadup homie" });
 
-	console.log('aws accessKeyId: ', process.env.AWS_KEYID);
-	console.log('aws secretAccessKey: ', process.env.AWS_SECRET);
-	console.log('aws region: ', process.env.AWS_REGION);
 	var s3 = new AWS.S3({
 		accessKeyId: process.env.AWS_KEYID,
 		secretAccessKey: process.env.AWS_SECRET,
 		region: process.env.AWS_REGION
 	});
+	var length = Object.keys(req.files).length;
+	var count = 0;
 
-	Object.keys(req.files).map((file) => {
-		console.log("file: ", req.files[file]);
-		let pic = req.files[file];
+
+	// instead of doing this, we should make it so that we wait until the previous request
+	// has returned, so we don't overwhelm the s3 server
+	var fileNames = Object.keys(req.files);
+	var sendFile = (count) => {
+		let pic = req.files[fileNames[count]];
 		var params = {
 			Bucket: 'erica-charlie-pics-test',
 			Key: pic.name,
 			Body: pic.data
 		}
+		console.log('sending: ', pic.name);
 		s3.putObject(params, (err, data) => {
 			if(err) {
-				console.log("error: ", err);
+				console.log('error: ', err);
 			} else {
-				console.log("Success! Data: ", data);
+				console.log('success');
+				count++;
+				if(count === length) {
+					res.status(202).json({ message: "wadup homie" });
+				} else {
+					sendFile(count)
+				}
 			}
 		})
-	});
+	}
+
+	sendFile(count);
 
 
 	// save pics info to aws
